ed71af8eb81a7e34dc86ec4ba247240c42c4eb0f
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-07 01:14:57 -0500

    bump

--- test-project/project.clj
+++ test-project/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[com.ambrosebs/criterium-from-java "1.1.0-SNAPSHOT"]]
+  :dependencies [[com.ambrosebs/criterium-from-java "1.1.1-SNAPSHOT"]]
   :main example_benchmark.Main
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
