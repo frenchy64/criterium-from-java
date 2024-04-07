eca5a352daef3694fb0509849815248d70312df2
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-07 00:29:40 -0500

    serialized

--- java/com/ambrosebs/criterium_from_java/Criterium.java
+++ java/com/ambrosebs/criterium_from_java/Criterium.java
@@ -20,7 +20,7 @@ import java.util.concurrent.Callable;
  * "tail-quantile" - default 0.025
  * "bootstrap-size" - default 1000, 500 for quick benchmarks
  * "overhead" - If you want consistency across JVM processes, set this to a constant value.
- * "final-gc-problem-threshold" - Fraction of excution time allowed for final cleanup before a warning is issued. Default: 0.01.
+ * "final-gc-problem-threshold" - Fraction of execution time allowed for final cleanup before a warning is issued. Default: 0.01.
  * "use-mxbean-for-times" - Default false
  *
  * Output results:
@@ -28,6 +28,7 @@ import java.util.concurrent.Callable;
  * "os-details" - a Map of "arch", "available-processors", "name", "version", sourced from java.lang.management.ManagementFactory.getOperatingSystemMXBean()
  * "execution-count" - a Long
  * "options" - the options used for this run, including the defaults. can be passed back as config to bench(Callable, Map) to repeat benchmark.
+ * "serialized-options" - the options used for this run as a serialized string. can be passed back as config to bench(Callable, String) to repeat benchmark.
  * "sample-mean" - a Double
  * "mean" - a Long
  * "tail-quantile" - a Double
@@ -54,10 +55,19 @@ public class Criterium {
         return 10*2;
       }
     });
-    System.out.println("Returns a map of results for programmatic manipulation.");
+    System.out.println("\nReturns a map of results for programmatic manipulation.");
     System.out.println("For example, benchmark run 5 return this value:");
     List results = (List)benchResults.get("results");
     System.out.println(results.get(4).toString());
+
+    System.out.println("\nWe can reproduce the same benchmark by copy-pasting the above serialized config string into the Java program.");
+
+    Map reproducedOptions = bench(new Callable<Integer>() {
+      public Integer call() throws InterruptedException {
+        Thread.sleep(100);
+        return 10*2;
+      }
+    },  "{\"quick\" true, \"max-gc-attempts\" 100, \"samples\" 6, \"target-execution-time\" 100000000, \"warmup-jit-period\" 5000000000, \"tail-quantile\" 0.025, \"bootstrap-size\" 500, \"overhead\" 8.253210752367998E-9}");
   }
 
   /**
@@ -99,17 +109,21 @@ public class Criterium {
     return runBenchmark(r, config);
   }
 
+  public static Map<Object,Object> bench(Callable r, String config) {
+    return runBenchmark(r, config);
+  }
 
 
 
 
   //Clojure boilerplate...
-  private static Map<Object,Object> runBenchmark(Callable c, Map<Object,Object> config) {
+  private static Map<Object,Object> runBenchmark(Callable c, Object config) {
     // if you see reflection warnings, there's a problem and results will not be valid
     eval("(alter-var-root #'*warn-on-reflection* (fn [_] true))");
     IFn runner = (IFn)eval(
         "(fn [^Callable c s-opts]"+
-        "  (let [{:keys [progress quick debug warn max-gc-attempts] :as opts} (update-keys s-opts keyword)"+
+        "  (let [s-opts (if (string? s-opts) (edn/read-string s-opts) (into {} s-opts))"+
+        "        {:keys [progress quick debug warn max-gc-attempts final-gc-problem-threshold] :as opts} (update-keys s-opts keyword)"+
         "        benchmark (if quick b/quick-benchmark* b/benchmark*)"+
         "        res (with-bindings (cond-> {}"+
         "                             progress (assoc #'b/*report-progress* true)"+
@@ -119,10 +133,12 @@ public class Criterium {
         "                             warn (assoc #'b/*report-warn* true))"+
         "              (benchmark #(.call c) (dissoc opts :progress :quick :debug :warn :final-gc-problem-threshold)))"+
         "        _ (b/report-result res)"+
-        "        stringified (-> (walk/postwalk (fn [v] (cond-> v (map? v) (update-keys name) (sequential? v) vec)) (dissoc res :results))"+
-        "                        (assoc \"results\" (vec (:results res)))"+
-        "                        (update \"options\" into s-opts))]"+
+        "        {:strs [options] :as stringified} (-> (walk/postwalk (fn [v] (cond-> v (map? v) (update-keys name) (sequential? v) vec)) (dissoc res :results))"+
+        "                                              (assoc \"results\" (vec (:results res)))"+
+        "                                              (update \"options\" #(into s-opts %)))"+
+        "        {:strs [serialized-options] :as stringified} (assoc stringified \"serialized-options\" (pr-str options))]"+
         "    (flush)"+ //IMPORTANT!!! results don't get printed otherwise
+        "    (println \"To reproduce via bench(Callable, String), use:\" (pr-str serialized-options))"+
         "    stringified))");
     return (Map)runner.invoke(c, config);
   }
@@ -137,6 +153,6 @@ public class Criterium {
   private static Object eval(String s) {
     IFn evalVar = Clojure.var("clojure.core", "eval");
     IFn prStrVar = Clojure.var("clojure.core", "pr-str");
-    return evalVar.invoke(read("(clojure.core/binding [clojure.core/*ns* (clojure.core/create-ns 'crit-bench.main)] (clojure.core/eval '(clojure.core/ns crit-bench.main (:require [clojure.walk :as walk] [clojure.pprint :as pp] [criterium.core :as b]) (:import [java.util.concurrent Callable]))) (clojure.core/eval (clojure.core/read-string "+(String)prStrVar.invoke(s)+")))"));
+    return evalVar.invoke(read("(clojure.core/binding [clojure.core/*ns* (clojure.core/create-ns 'crit-bench.main)] (clojure.core/eval '(clojure.core/ns crit-bench.main (:require [clojure.edn :as edn] [clojure.walk :as walk] [criterium.core :as b]) (:import [java.util.concurrent Callable]))) (clojure.core/eval (clojure.core/read-string "+(String)prStrVar.invoke(s)+")))"));
   }
 }
